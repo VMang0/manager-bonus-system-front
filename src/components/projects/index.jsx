ee16ce0da858e864af32f4d0d17cd9c9ab2e2aab
VMang0 <valeriakorolkova2703@gmail.com>
2023-10-29 15:21:47 +0300

    add main component for projects page

---
+++ src/components/projects/index.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  ProjectStatistic,
+  StatisticItem,
+  StyledProjectsComponent,
+} from './style';
+import { Grid, Typography } from '@mui/material';
+import { Projects } from '../../common/moks/projects/projects';
+import ProjectItem from '../progectItem';
+import { statistic } from '../../common/moks/statistic';
+import { HorizontalRule } from '@mui/icons-material';
+import FlexBetween from '../flex-between';
+
+const ProjectsComponent = () => {
+  return (
+    <StyledProjectsComponent>
+      <Grid container className='grid-container'>
+        {Projects.map((project, index) => (
+          <Grid item key={index} className='adaptive-grid'>
+            <ProjectItem project={project} />
+          </Grid>
+        ))}
+      </Grid>
+      <ProjectStatistic>
+        <Typography className='statistic-text'>Statistic:</Typography>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          {statistic.map((item, i) => (
+            <Grid key={i} item xs={6}>
+              <StatisticItem name={item.name}>
+                {item.name}
+                <FlexBetween className='statistic-item'>
+                  <HorizontalRule className='statistic-icon' />
+                  <Typography className='statistic-number'>
+                    {item.number}
+                  </Typography>
+                </FlexBetween>
+              </StatisticItem>
+            </Grid>
+          ))}
+        </Grid>
+      </ProjectStatistic>
+    </StyledProjectsComponent>
+  );
+};
+
+export default ProjectsComponent;
---
+++ src/components/projects/style.js
@@ -0,0 +1,74 @@
+import { styled } from '@mui/system';
+import { Box } from '@mui/material';
+import { tokens } from '../../theme';
+
+export const StyledProjectsComponent = styled(Box)(({ theme }) => ({
+  minHeight: '100%',
+  display: 'grid',
+  gridTemplateColumns: '1fr minmax( auto, 18.4%)',
+  gridGap: '10px',
+  paddingLeft: '10px',
+  '.grid-container': {
+    background: `${
+      theme.palette.mode === 'dark'
+        ? tokens(theme.palette.mode).black['500']
+        : tokens(theme.palette.mode).grey['100']
+    }`,
+    borderRadius: '20px 20px 0 0',
+    padding: '30px',
+    margin: 0,
+    display: 'grid',
+    gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
+    gridGap: '1.5rem',
+    gridAutoRows: '300px',
+  },
+  '.adaptive-grid': {
+    height: 'auto',
+  },
+}));
+
+export const ProjectStatistic = styled(Box)(() => ({
+  padding: '10px',
+  '.statistic-text': {
+    fontWeight: '700',
+    paddingBottom: '20px',
+  },
+}));
+
+export const StatisticItem = styled(Box)(({ theme, name }) => ({
+  fontSize: '12px',
+  '.statistic-item': {
+    justifyContent: 'normal',
+    paddingTop: '5px',
+  },
+  '.statistic-number': {
+    fontSize: '22px',
+    fontWeight: '700',
+    paddingLeft: '10px',
+  },
+  '.statistic-icon': {
+    transform: 'rotate(90deg)',
+    fontSize: '30px',
+    ...(name === 'Completed'
+      ? {
+          color: `rgb(${tokens(theme.palette.mode).status.COMPLETED})`,
+        }
+      : {
+          ...(name === 'In progress'
+            ? {
+                color: `rgb(${tokens(theme.palette.mode).status.INPROGRESS})`,
+              }
+            : {
+                ...(name === 'Off track'
+                  ? {
+                      color: `rgb(${
+                        tokens(theme.palette.mode).status.OFFTRACK
+                      })`,
+                    }
+                  : {
+                      color: tokens(theme.palette.mode).darkBlue['200'],
+                    }),
+              }),
+        }),
+  },
+}));
