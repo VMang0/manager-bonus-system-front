fb84320c2858df3097e33d902ea76c41152eb08d
VMang0 <valeriakorolkova2703@gmail.com>
2023-10-29 15:21:01 +0300

    static info for debug

---
+++ src/common/moks/projects/projects.js
@@ -0,0 +1,62 @@
+export const Projects = [
+  {
+    name: 'United Health Group',
+    description: 'Company Branding',
+    status: 'Completed',
+    priority: 'High',
+  },
+  {
+    name: 'United Health Group',
+    description: 'Company Branding',
+    status: 'Completed',
+    priority: 'High',
+  },
+  {
+    name: 'United Health Group',
+    description: 'Company Branding',
+    status: 'Off track',
+    priority: 'High',
+  },
+  {
+    name: 'United Health Group',
+    description: 'Company Branding',
+    status: 'In progress',
+    priority: 'High',
+  },
+  {
+    name: 'United Health Group',
+    description: 'Company Branding',
+    status: 'Completed',
+    priority: 'High',
+  },
+  {
+    name: 'United Health Group',
+    description: 'Company Branding',
+    status: 'Completed',
+    priority: 'High',
+  },
+  {
+    name: 'United Health Group',
+    description: 'Company Branding',
+    status: 'Completed',
+    priority: 'High',
+  },
+  {
+    name: 'United Health Group',
+    description: 'Company Branding',
+    status: 'Completed',
+    priority: 'High',
+  },
+  {
+    name: 'United Health Group',
+    description: 'Company Branding',
+    status: 'Completed',
+    priority: 'High',
+  },
+  {
+    name: 'United Health Group',
+    description: 'Company Branding',
+    status: 'Completed',
+    priority: 'High',
+  },
+];
---
+++ src/common/moks/statistic/index.js
@@ -0,0 +1,18 @@
+export const statistic = [
+  {
+    name: 'Projects',
+    number: 123,
+  },
+  {
+    name: 'Completed',
+    number: 123,
+  },
+  {
+    name: 'In progress',
+    number: 123,
+  },
+  {
+    name: 'Off track',
+    number: 123,
+  },
+];
