5e3865b320e63b01d058b062f497ec7080cc1955
VMang0 <valeriakorolkova2703@gmail.com>
2023-11-12 20:47:48 +0300

    add: users for activate component

---
+++ src/components/users-for-activate/style.js
@@ -0,0 +1,11 @@
+import { styled } from '@mui/system';
+import { Box } from '@mui/material';
+
+export const UsersForActivateStyled = styled(Box)(() => ({
+  display: 'flex',
+  justifyContent: 'space-between',
+  padding: '0 15px',
+  '.UserInfoForm': {
+    width: '48%',
+  },
+}));
