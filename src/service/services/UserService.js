1e34e31ee8888e5afa5859bc03ed6dc0012dd9d1
VMang0 <valeriakorolkova2703@gmail.com>
2023-11-14 00:28:18 +0300

    add: request for verify manager in service

--- src/service/services/UserService.js
+++ src/service/services/UserService.js
@@ -14,9 +14,30 @@ export default class UserService {
     }
   }
 
+  static async addManager(email, company) {
+    try {
+      const response = await $api.post(`/add/manager`, { email, company });
+      return response;
+    } catch (e) {
+      throw e.response?.data?.message;
+    }
+  }
   static async verification(userInfo) {
     try {
       const response = await $api.post(`/user/verify`, userInfo);
+      return response;
+    } catch (e) {
+      throw e.response?.data?.message;
+    }
+  }
+  static async verifyManager(link, email, password, company) {
+    try {
+      const response = await $api.post(`/manager/verify`, {
+        link,
+        email,
+        password,
+        company,
+      });
       return response;
     } catch (e) {
       throw e.response?.data?.message;
