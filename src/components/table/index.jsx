ea20c212ec3393be4803b2dab5b4f7725ae3fd81
VMang0 <valeriakorolkova2703@gmail.com>
2023-11-14 00:29:33 +0300

    edit: change table

--- src/components/table/index.jsx
+++ src/components/table/index.jsx
@@ -1,36 +1,27 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { RowTables } from './style';
+import React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import { TableContainer } from './style';
 
-const TableComponent = ({ header, items, setItem }) => {
+const TableComponent = ({ header, items, setItem, checkbox = false }) => {
+  const handleRowClick = (params) => {
+    setItem(params.row);
+  };
+
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            {header?.map((item, index) => (
-              <TableCell key={index}>{item.headerName}</TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {items?.map((item, index) => (
-            <RowTables key={index} onClick={() => setItem(item)}>
-              {header.map((header_item, index) => (
-                <TableCell key={index} component='th' scope='row'>
-                  {item[header_item.field]}
-                </TableCell>
-              ))}
-            </RowTables>
-          ))}
-        </TableBody>
-      </Table>
+    <TableContainer>
+      <DataGrid
+        rows={items}
+        columns={header}
+        getRowId={(row) => row._id}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+        onRowClick={handleRowClick}
+        checkboxSelection={checkbox}
+      />
     </TableContainer>
   );
 };
