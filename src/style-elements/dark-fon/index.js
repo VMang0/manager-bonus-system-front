99cb630931c074ea37402208a7dc9b75fc8fc3fc
VMang0 <valeriakorolkova2703@gmail.com>
2023-11-14 23:07:36 +0300

    add: dark fon for forms

---
+++ src/style-elements/dark-fon/index.js
@@ -0,0 +1,17 @@
+import { styled } from '@mui/system';
+import { Box } from '@mui/material';
+
+const DarkFon = styled(Box)({
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  zIndex: 10000,
+  minHeight: '100%',
+  minWidth: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  backdropFilter: 'blur(1px)',
+});
+export default DarkFon;
