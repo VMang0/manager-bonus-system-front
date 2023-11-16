b29e31b179daa379e6c7fc9dde8fc420d7c5cf8b
VMang0 <valeriakorolkova2703@gmail.com>
2023-11-17 02:12:27 +0300

    add: header for employees table

--- src/common/moks/table-headers/index.js
+++ src/common/moks/table-headers/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Avatar } from '@mui/material';
 
+//таблица активации пользователей
 export const tableHeaderActivateUsers = [
   { field: 'email', headerName: 'Email', width: 300 },
   { field: 'role', headerName: 'Role', width: 200 },
 ];
 
+//таблица менеджеров
 export const tableHeaderManagers = [
   {
     field: 'number',
@@ -27,4 +29,20 @@ export const tableHeaderManagers = [
   { field: 'company', headerName: 'Company', width: 150 },
   { field: 'position', headerName: 'Position', width: 180 },
   { field: 'date', headerName: 'Date', width: 150 },
+];
+
+//таблица с сотрудниками при создании проекта
+export const tableHeaderEmployees = [
+  {
+    field: 'image',
+    headerName: '',
+    renderCell: (params) => <Avatar alt='profile' src={params.row.image} />,
+    sortable: false,
+    filterable: false,
+    width: 70,
+  },
+  { field: 'name', headerName: 'Name', width: 100 },
+  { field: 'lastname', headerName: 'Lastname', width: 100 },
+  { field: 'position', headerName: 'Position', width: 100 },
+  { field: 'rang', headerName: 'Rang', width: 100 },
 ];
